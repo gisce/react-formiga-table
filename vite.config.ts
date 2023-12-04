b82485c22ee03f903f42c2ddb0fc99636795d257
Marc Güell Segarra <marc@ondori.dev>
2023-12-04 08:45:36 +0100

    New alpha version

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,35 @@
-import react from "@vitejs/plugin-react";
-import * as path from "path";
-import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { PluginOption, defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    peerDepsExternal({
+      includeDependencies: true,
+    }) as PluginOption,
     react(),
     dts({
       insertTypesEntry: true,
     }),
+    viteTsconfigPaths(),
   ],
   build: {
-    sourcemap: "inline",
+    emptyOutDir: true,
+    sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "react-formiga-table",
-      formats: ["es", "umd"],
+      formats: ["es"],
       fileName: (format) => `react-formiga-table.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom", "styled-components", "use-deep-compare-effect"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "styled-components": "styled",
-        },
-      },
+      // We manually add these since they are not directly in package.json, but they end up in the bundle if we don't exclude them.
+      // Reason for this is using imports for specific components
+      external: [
+      ],
     },
   },
 });
